884d94f22c567a13c665cfa2e721441b353a5bb6
Tang <429434971@qq.com>
2019-03-22 16:45:16 +0800

    update

--- src/StringTest.java
+++ src/StringTest.java
@@ -1,11 +1,26 @@
 public class StringTest {
     public static void main(String[] args){
         String fragment = "abcdefghijklmnopqqrstuvwxyz";
-        int times = 10000;
+        int times = 100000;
 
         //通过String对象
         long timeStart1 = System.currentTimeMillis();
         String str1 = "";
+        for(int i=0;i<times;i++)
+        {
+            str1 += fragment;
+        }
+        long timeEnd1=System.currentTimeMillis();
+        System.out.println("String: " + (timeEnd1 - timeStart1) + "ms");
+
+        //通过StringBuffer
+        long timeStart2 = System.currentTimeMillis();
+        StringBuffer str2= new StringBuffer();
+        for(int i=0;i<times;i++){
+            str2.append(fragment);
+        }
+        long timeEnd2 = System.currentTimeMillis();
+        System.out.println("StringBuffer: " + (timeEnd2-timeStart2) +"ms");
 
     }
 
